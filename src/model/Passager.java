b754e6f9d76cf850dc8fc46ac28e7c39af3382a0
Jérémy Fontaine <jeremy.fontaine@live.fr>
2015-11-23 22:12:24 +0100

    Ajout constructeur Passager

--- src/model/Passager.java
+++ src/model/Passager.java
@@ -2,4 +2,21 @@ package model;
 
 public class Passager extends Profil {
 
+	public Passager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Passager(String pseudo, String pass, String email, String nom, String prenom, String dateInscription,
+			String dateNaissance, Ville ville, Preference preference) {
+		super(pseudo, pass, email, nom, prenom, dateInscription, dateNaissance, ville, preference);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Passager(Profil p) {
+		this(p.getPseudo(), p.getPass(), p.getEmail(), p.getNom(), p.getPrenom(), p.getDateInscription(), p.getDateNaissance(), p.getVille(), p.getPreference());
+		this.setId(p.getId());
+	}
+
+	
 }
