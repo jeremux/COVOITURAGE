a9b2c7304481ef2442563bd0c3a587a888223a61
Jérémy Fontaine <jeremy.fontaine@live.fr>
2015-11-27 12:52:50 +0100

    Pattern DAO
    Ajout de la classe abstraite DAO

---
+++ src/dao/DAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+
+import connexion.SingletonConnection;
+
+public abstract class DAO<T> {
+	
+	public Connection connection = SingletonConnection.getConnection();
+	
+	/**
+	 * Permet de récupérer un objet via son ID
+	 * @param id
+	 * @return
+	 */
+	public abstract T find(int id);
+	
+	/**
+	 * Permet de créer une entrée dans la base de données
+	 * par rapport à un objet
+	 * @param obj
+	 */
+	public abstract T create(T obj);
+	
+	/**
+	 * Permet de mettre à jour les données d'une entrée dans la base 
+	 * @param obj
+	 */
+	public abstract T update(T obj);
+	
+	/**
+	 * Permet la suppression d'une entrée de la base
+	 * @param obj
+	 */
+	public abstract void delete(T obj);
+}
