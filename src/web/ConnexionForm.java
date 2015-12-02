9b2afab0a61dbba953c00a55653b89ab8c49642c
Jérémy Fontaine <jeremy.fontaine@live.fr>
2015-12-02 19:22:12 +0100

    Gestion de la connexion

---
+++ src/web/ConnexionForm.java
@@ -0,0 +1,73 @@
+package web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import metier.ProfilGestionImpl;
+import model.Profil;
+
+public class ConnexionForm {
+
+	    private static final String CHAMP_EMAIL  = "email";
+	    private static final String CHAMP_PASS   = "motdepasse";
+
+	    private String              resultat;
+	    private Map<String, String> erreurs      = new HashMap<String, String>();
+
+	    public String getResultat() {
+	        return resultat;
+	    }
+
+	    public Map<String, String> getErreurs() {
+	        return erreurs;
+	    }
+
+	    public Profil connecterUtilisateur( HttpServletRequest request ) {
+	        /* Récupération des champs du formulaire */
+	        String email = getValeurChamp( request, CHAMP_EMAIL );
+	        String motDePasse = getValeurChamp( request, CHAMP_PASS );
+	        ProfilGestionImpl profilDAO = new ProfilGestionImpl();
+	        Profil utilisateur = new Profil();
+
+	        /* Validation */
+	        utilisateur = profilDAO.findByEmail(email);
+	        if (utilisateur.getId()==-1) 
+	        	setErreur(CHAMP_EMAIL, "Email non enregistré");
+	        else
+	        	if (!utilisateur.getPass().equals(motDePasse))
+	        		setErreur(CHAMP_PASS, "Mot de passe invalide");
+
+	        /* Initialisation du résultat global de la validation. */
+	        if ( erreurs.isEmpty() ) {
+	            resultat = "Succès de la connexion.";
+	        } else {
+	            resultat = "Échec de la connexion.";
+	        }
+	        
+	        return utilisateur;
+	    }
+
+
+
+	    /*
+	     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
+	     */
+	    private void setErreur( String champ, String message ) {
+	        erreurs.put( champ, message );
+	    }
+
+	    /*
+	     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+	     * sinon.
+	     */
+	    private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
+	        String valeur = request.getParameter( nomChamp );
+	        if ( valeur == null || valeur.trim().length() == 0 ) {
+	            return null;
+	        } else {
+	            return valeur;
+	        }
+	    }
+}
