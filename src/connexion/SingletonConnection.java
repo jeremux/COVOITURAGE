bebb455f1cea503334e2e7c5d91d2936c86229ba
Jérémy Fontaine <jeremy.fontaine@live.fr>
2015-12-10 17:06:48 +0100

    Pas de changement

--- src/connexion/SingletonConnection.java
+++ src/connexion/SingletonConnection.java
@@ -9,12 +9,12 @@ public class SingletonConnection {
 	private static Connection connection;
 	private static String pathDB="/Users/jeremux/Dropbox/Etudes/2015-2016/siad/eclipse/COVOITURAGE/WebContent/WEB-INF/db/";
 	private static String nomDB="covoiturage.db";
-	
+
 	static {
 		/**
-         * Chargement du pilote SQlite
-         */
-        try {
+		 * Chargement du pilote SQlite
+		 */
+		try {
 			Class.forName("org.sqlite.JDBC");
 			System.out.println("Pilote SQlite JDBC charge");
 		} catch (ClassNotFoundException e) {
@@ -22,18 +22,18 @@ public class SingletonConnection {
 			e.printStackTrace();
 			System.err.println("Erreur lors du chargmement du pilote");
 		}
-        
-        /**
-         * Connexion avec la base
-         */
-        try {
+
+		/**
+		 * Connexion avec la base
+		 */
+		try {
 			connection = DriverManager.getConnection("jdbc:sqlite:"+SingletonConnection.pathDB+nomDB);
 			System.out.println("Connexion operationnelle");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.err.println("Erreur lors de l etablissement de la connexion");
 		}
-		
+
 	}
 
 	public static Connection getConnection() {
